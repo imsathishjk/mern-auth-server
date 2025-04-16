0adc1a8c7684cc5dd8db3e9c1f5ae4d9216d62f6
imsathishjk <sathishjk01@gmail.com>
2025-04-16 23:00:40 +0530

    "Front-end URL Updated"

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,7 @@ export const userRegsiter = async (req, res) => {
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
-        res.setHeader('Access-Control-Allow-Origin', 'https://your-frontend.vercel.app');
+        res.setHeader('Access-Control-Allow-Origin', 'https://mern-auth-client-flame.vercel.app');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
         return res.json({ success: true, message: 'User Registered Successfully' })
     } catch (error) {
@@ -58,7 +58,7 @@ export const userLogin = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
-        res.setHeader('Access-Control-Allow-Origin', 'https://your-frontend.vercel.app');
+        res.setHeader('Access-Control-Allow-Origin', 'https://mern-auth-client-flame.vercel.app');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
 
         if (isPassMatch) {
