9970cc9edc2e5e4956ee8de5cfb905037092aafc
imsathishjk <sathishjk01@gmail.com>
2025-04-16 19:23:00 +0530

    Initial Commit

---
+++ middleware/userAuth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+const userAuth = async (req, res, next) => {
+    const { token } = req.cookies;
+    console.log(req.cookies.token);
+    try {
+        console.log('token:', token)
+        if (!token) {
+            console.log('token:', token)
+            return res.json({ success: false, message: 'Not Authorized Login Again!' });
+        }
+        const decodeToken = jwt.verify(token, process.env.JWT_SECRET_TEXT);
+        if (decodeToken.id) {
+           
+            req.userId = decodeToken.id;
+
+        } else {
+            console.log('token2:', token)
+            return res.json({ success: false, message: 'Not Authorized Login Again!' });
+        }
+        return next();
+
+
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+}
+export default userAuth;
---
+++ server.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import dotenv from 'dotenv/config';
+import authRouter from './routes/authRoute.js';
+import { connectDB } from './config/db.js';
+import userRouter from './routes/userRoute.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+
+// Initiating express App
+const app = express();
+
+const PORT = process.env.PORT || 5000
+const BACKEND_URL = process.env.BACKEND_URL;
+
+// Connect Database
+await connectDB();
+
+
+// Middileware to access token
+
+
+const allowedOrigins = [
+    'http://localhost:5173' // ✅ Your Vercel frontend URL
+    // 
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('CORS not allowed'));
+        }
+    },
+    credentials: true, // ✅ Important for sending cookies
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+// Middleware to extract data from the user
+app.use(cookieParser());
+
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server running on ${BACKEND_URL}${PORT}...`)
+});
+
+
+app.get('/', (req, res) => {
+    res.send('API Working..')
+});
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
